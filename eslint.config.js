ba651d6b6d2cd6c7a18dc3762617e6ac04f68ae7
Miklos Daniel <daniel.miklos0@gmail.com>
2025-11-07 21:39:49 +0100

    fix: remove depricated eslint config constructor

--- eslint.config.js
+++ eslint.config.js
@@ -4,8 +4,9 @@ import svelte from "eslint-plugin-svelte";
 import globals from "globals";
 import ts from "typescript-eslint";
 import svelteConfig from "./svelte.config.js";
+import { defineConfig } from "eslint/config";
 
-export default ts.config(
+export default defineConfig(
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs.recommended,
